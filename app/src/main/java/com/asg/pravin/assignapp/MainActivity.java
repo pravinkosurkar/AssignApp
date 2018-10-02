968e5c13b0d537408635d352a2f75b7b3ce1c2ac
Pravin Kosurkar <pravinkosurkar@gmail.com>
2018-10-02 19:54:56 +0530

    second commit

---
+++ app/src/main/java/com/asg/pravin/assignapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.asg.pravin.assignapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.asg.pravin.assignapp.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/asg/pravin/assignapp/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.asg.pravin.assignapp.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
